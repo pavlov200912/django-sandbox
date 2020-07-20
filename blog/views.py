d70cdaf846281f45b71a5e8226ac9bf25edb2ba3
Pavlov Ivan <pavlov200912@gmail.com>
2020-07-20 17:59:56 +0300

    Girls blog with CSS and stuff

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-import datetime
+from .models import Post
+from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+
 
 # Create your views here.
 
 def post_list(request):
-    #now = datetime.datetime.now()
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
-    data1 = datetime.datetime.now()
-    data2 = datetime.datetime(2000, 10, 8)
 
-    diff = data1 - data2
-
-    days, seconds = diff.days, diff.seconds
-    hours = days * 24 + seconds // 3600
-
-    html = "<html><body><p>Илья Дорошенко</p> <p><strong>Пи-До-Рас</strong> уже %s часов.</p></body></html>" % str(hours)
-    return HttpResponse(html)
-    #return render(request, 'blog/post_list.html', {})
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
